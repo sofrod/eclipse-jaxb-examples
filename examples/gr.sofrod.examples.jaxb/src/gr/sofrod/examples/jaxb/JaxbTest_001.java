6a0f4f1fafda971bc834a0feeec0af05aa5af068
sofrod <sofronis@gmail.com>
2015-11-20 20:59:57 +0100

    jaxb examples project

---
+++ examples/gr.sofrod.examples.jaxb/src/gr/sofrod/examples/jaxb/JaxbTest_001.java
@@ -0,0 +1,14 @@
+package gr.sofrod.examples.jaxb;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JaxbTest_001 {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
